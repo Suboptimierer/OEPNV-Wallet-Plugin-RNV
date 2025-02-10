b5fb1ab94dc8a1ca4b21f84e91e0925f01d51651
Jonas Sannewald <github.zgxjfq@sannewald.email>
2025-02-10 04:28:09 +0100

    Plugin umbenannt

---
+++ Sources/OEPNV-Wallet-Plugin-RNV/PluginRNV.swift
@@ -0,0 +1,15 @@
+//
+//  PluginRNV.swift
+//  OEPNV-Wallet-Plugin-RNV
+//
+//  Created by Jonas Sannewald on 10.02.25.
+//
+
+import OEPNVWalletPluginAPI
+
+public struct PluginRNV: OEPNVWalletPlugin {
+    
+    public let associationName = "Rhein-Neckar-Verkehr GmbH"
+    public let associationAbbreviation = "rnv"
+    
+}
