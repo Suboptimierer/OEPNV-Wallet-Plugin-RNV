9dcf0d56d776283ea61e9554f92045e95b54752b
Jonas Sannewald <github.zgxjfq@sannewald.email>
2025-03-30 00:52:12 +0100

    Notwendige Endpunkte der API hinzugefügt

--- Tools/x-api-signature-generator.playground/Contents.swift
+++ Tools/x-api-signature-generator.playground/Contents.swift
@@ -2,29 +2,63 @@ import Foundation
 import CryptoKit
 
 
-// MARK: - Eingabewerte
-
-let secret = "6xVKJuwLEwFhAwRSz2Ed"
-
-let body = ""
+// MARK: - Gleich für jeden Endpunkt
+let secret = ""
 let host = "tickets.rnv-online.de"
 let port = "443"
-let path = "/index.php/mobileService/resource_list"
-let eosDate = "Fri, 28 Mar 2025 22:57:00 GMT"
-let contentType = ""
-let authorization = ""
+let contentType = "application/json"
 let anonymous = ""
 let sso = ""
-let userAgent = "RNV AND/3.22.0/2022.03/rnv-live (Google emu64a - 'sdk_gphone64_arm64'; Android; 13, SDK: 33)"
+let userAgent = "RNV AND/3.22.0/2022.03/rnv-live (Google komodo - 'Pixel 9'; Android; 15, SDK: 35)"
+
+
+/*
+// MARK: - Endpunkt Login
+let body = """
+{"credentials":{"password":"<password>","username":"<email>"}}
+"""
+let path = "/index.php/mobileService/login"
+let eosDate = "Sat, 29 Mar 2025 20:17:00 GMT"
+let authorization = ""
+*/
+
+
+/*
+// MARK: - Endpunkt Logout
+let body = """
+{}
+"""
+let path = "/index.php/mobileService/logout"
+let eosDate = "Sat, 29 Mar 2025 20:25:00 GMT"
+let authorization = "TICKeos <token>"
+*/
+
+
+/*
+// MARK: - Endpunkt Sync
+let body = """
+{"anonymous_tickets":[]}
+"""
+let path = "/index.php/mobileService/sync"
+let eosDate = "Sat, 29 Mar 2025 20:40:00 GMT"
+ let authorization = "TICKeos <token>"
+*/
+
+
+// MARK: - Endpunkt Ticket
+let body = """
+{"tickets":["<ticket-id>"],"details":true,"provide_aztec_content":false,"parameters":false}
+"""
+let path = "/index.php/mobileService/ticket"
+let eosDate = "Sat, 29 Mar 2025 20:52:00 GMT"
+let authorization = "TICKeos <token>"
 
 
 // MARK: - Body hashen
-
 let hashedBody = hmacSHA512(body, key: secret)
 
 
 // MARK: - Rohdaten der Signatur zusammenbauen
-
 let signature = [
     hashedBody,
     host,
@@ -40,13 +74,11 @@ let signature = [
 
 
 // MARK: - Rohdaten der Signatur zusammenbauen
-
 let apiSignature = hmacSHA512(signature, key: secret)
 print(apiSignature)
 
 
 // MARK: - Hilfsfunktionen
-
 func hmacSHA512(_ message: String, key: String) -> String {
     let keyData = Data(key.utf8)
     let messageData = Data(message.utf8)
